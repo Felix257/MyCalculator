b1d93fb70c5291b6b13f8fac7a5055c88a67f74b
unknown <felix.ing86@gmail.com>
2018-03-26 14:33:01 -0400

    next

--- app/src/main/java/com/example/felix/mycalc/MainActivity.java
+++ app/src/main/java/com/example/felix/mycalc/MainActivity.java
@@ -21,26 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        add = (Button) findViewById(R.id.add);
-        sub = (Button) findViewById(R.id.sub);
-        div = (Button) findViewById(R.id.div);
-        mul = (Button) findViewById(R.id.mul);
-        equal = (Button) findViewById(R.id.equal);
-        one = (Button) findViewById(R.id.one);
-        two = (Button) findViewById(R.id.two);
-        three = (Button) findViewById(R.id.three);
-        four = (Button) findViewById(R.id.four);
-        five = (Button) findViewById(R.id.five);
-        six = (Button) findViewById(R.id.six);
-        seven = (Button) findViewById(R.id.seven);
-        eight = (Button) findViewById(R.id.eight);
-        nine = (Button) findViewById(R.id.nine);
-        zero = (Button) findViewById(R.id.zero);
-        del = (Button) findViewById(R.id.del);
-        clear = (Button) findViewById(R.id.clear);
-        percent = (Button) findViewById(R.id.percent);
-        bracket = (Button) findViewById(R.id.bracket);
-
         screen = (TextView) findViewById(R.id.screen);
         screen.setText(display);
 
